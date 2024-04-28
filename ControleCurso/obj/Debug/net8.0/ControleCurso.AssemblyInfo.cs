0c4593d5fdab0debbb53ea6a3322c4a8b0ee637f
Yasmin Pereira Vargas <y.pereira.vargas@avanade.com>
2024-04-28 11:34:31 -0300

    updated project

--- ControleCurso/obj/Debug/net8.0/ControleCurso.AssemblyInfo.cs
+++ ControleCurso/obj/Debug/net8.0/ControleCurso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleCurso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d5cd1253d7f1d23c44e8696a2ebb8c859f111b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleCurso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleCurso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
